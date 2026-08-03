59b81b327d139005b16adb279f2c3adef034cde2
belt8711 <suarezjet@gmail.com>
2026-08-03 16:19:45 -0400

    tmp

--- spotify.js
+++ spotify.js
@@ -1,79 +1,109 @@
 require("dotenv").config();
 
 async function userCurrent(req, res) {
-	try {
-		const refreshToken = req.body.token;
+try {
+	const refreshToken = req.body.token;
 
-		if (!refreshToken) {
-			return res.status(400).json({
-				error: "Missing token"
-			});
-		}
-
-		const response = await fetch(
-			"https://accounts.spotify.com/api/token",
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-					"Authorization":
-						"Basic " +
-						Buffer.from(
-							`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
-						).toString("base64")
-				},
-				body: new URLSearchParams({
-					grant_type: "refresh_token",
-					refresh_token: refreshToken
-				})
-			}
-		);
-
-		const tokenData = await response.json();
-
-		if (!response.ok) {
-			throw new Error(JSON.stringify(tokenData));
-		}
-
-		const accessToken = tokenData.access_token;
-
-		const spotifyResponse = await fetch(
-			"https://api.spotify.com/v1/me/player/currently-playing",
-			{
-				headers: {
-					Authorization: `Bearer ${accessToken}`
-				}
-			}
-		);
-
-		if (spotifyResponse.status === 204) {
-			return res.json({
-				online: false
-			});
-		}
-
-		const data = await spotifyResponse.json();
-
-		res.json({
-			online: true,
-			song: data.item.name,
-			songUrl: data.item.external_urls.spotify,
-			artist: data.item.artists[0].name,
-			artistUrl: data.item.artists[0].external_urls.spotify,
-			album: data.item.album.name,
-			albumUrl: data.item.album.external_urls.spotify,
-			img: data.item.album.images[1]?.url || ""
-		});
-
-	} catch (err) {
-		console.error(err);
-
-		res.status(500).json({
-			error: err.message
-		});
+	if (!refreshToken) {
+	return res.status(400).json({
+		error: "Missing token"
+	});
 	}
+
+	// Get access token
+	const response = await fetch(
+	"https://accounts.spotify.com/api/token",
+	{
+		method: "POST",
+		headers: {
+		"Content-Type": "application/x-www-form-urlencoded",
+		"Authorization":
+			"Basic " +
+			Buffer.from(
+			`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+			).toString("base64")
+		},
+		body: new URLSearchParams({
+		grant_type: "refresh_token",
+		refresh_token: refreshToken
+		})
+	}
+	);
+
+	const tokenData = await response.json();
+
+	if (!response.ok) {
+	throw new Error(JSON.stringify(tokenData));
+	}
+
+	const accessToken = tokenData.access_token;
+
+	// Check currently playing first
+	const spotifyResponse = await fetch(
+	"https://api.spotify.com/v1/me/player/currently-playing",
+	{
+		headers: {
+		Authorization: `Bearer ${accessToken}`
+		}
+	}
+	);
+
+	// If currently playing, return that
+	if (spotifyResponse.status !== 204) {
+	const data = await spotifyResponse.json();
+
+	return res.json({
+		online: true,
+		song: data.item.name,
+		songUrl: data.item.external_urls.spotify,
+		artist: data.item.artists[0].name,
+		artistUrl: data.item.artists[0].external_urls.spotify,
+		album: data.item.album.name,
+		albumUrl: data.item.album.external_urls.spotify,
+		img: data.item.album.images[1]?.url || "",
+	});
+	}
+
+	// If nothing playing, get last played
+	const lastResponse = await fetch(
+	"https://api.spotify.com/v1/me/player/recently-played?limit=1",
+	{
+		headers: {
+		Authorization: `Bearer ${accessToken}`
+		}
+	}
+	);
+
+	const lastData = await lastResponse.json();
+
+	if (!lastData.items || lastData.items.length === 0) {
+	return res.json({
+		online: false
+	});
+	}
+
+	const item = lastData.items[0].track;
+
+	res.json({
+	online: false,
+	song: item.name,
+	songUrl: item.external_urls.spotify,
+	artist: item.artists[0].name,
+	artistUrl: item.artists[0].external_urls.spotify,
+	album: item.album.name,
+	albumUrl: item.album.external_urls.spotify,
+	img: item.album.images[1]?.url || "",
+	});
+
+} catch (err) {
+	console.error(err);
+
+	res.status(500).json({
+	error: err.message
+	});
+}
 }
 
 module.exports = {
-	userCurrent
+userCurrent
 };
